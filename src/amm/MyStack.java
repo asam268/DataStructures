92b346ab0a659ca123ee6b5ad9065cadbc8d4500
Asa Marshall <asam268@gmail.com>
2021-02-13 03:16:50 -0500

    update comments on MyStack

--- src/amm/MyStack.java
+++ src/amm/MyStack.java
@@ -1,28 +1,54 @@
 package amm;
 
-public class MyStack {
+/**
+ * MyStack is a statically sized stack of Strings
+ */
+class MyStack {
 
-    private final int MAX = 1000;
-    private int top;
-    private String[] stack = new String[MAX];
+    private final int MAX = 1000;               // default size of the stack
+    private int top;                            // The "Parser" which marks the top of the stack
+    private String[] stack = new String[MAX];   // array to hold values in the stack
 
-    public MyStack() {
+    MyStack() {
         top = -1;
     }
 
-    public boolean isEmpty() {
+    /**
+     * Return true if the stack is empty
+     *
+     * @return boolean
+     */
+    boolean isEmpty() {
         return (top < 0);
     }
 
-    public String peek() {
+    /**
+     * Shows the String at the top of the stack
+     *
+     * @return String or null if the stack is empty
+     */
+    String peek() {
         return (top < 0) ? null : stack[top];
     }
 
-    public String pop() {
+    /**
+     * Remove and return String at the top of the stack
+     *
+     * @return String or null to mark underflow condition
+     */
+    String pop() {
         return (top < 0) ? null : stack[top--];
     }
 
-    public boolean push(String value) {
+    /**
+     * Add an item to the top of the stack
+     * Return true if the item is successfully pushed
+     * Return false to mark an overflow condition
+     *
+     * @param value to be pushed to the top of the stack
+     * @return      boolean
+     */
+    boolean push(String value) {
         if (top >= (MAX - 1)) {
             return false;
         }
