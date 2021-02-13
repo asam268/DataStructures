830e5faca0e5f01ca11fc19e34d4b516fac0c5c3
Asa Marshall <asam268@gmail.com>
2021-02-13 03:10:02 -0500

    update comments on MyDynamicStack

--- src/amm/MyDynamicStack.java
+++ src/amm/MyDynamicStack.java
@@ -1,24 +1,44 @@
 package amm;
 
-public class MyDynamicStack {
+/**
+ * @author Asa Marshall
+ *
+ * MyDynamicStack is a resizable stack of Strings
+ */
+class MyDynamicStack {
 
-    private String[] stack;
-    private int top;
+    private String[] stack; // array to hold values in the stack
+    private int top;        // The "Parser" which marks the top of the stack
 
-    public MyDynamicStack() {
+    MyDynamicStack() {
         stack = new String[0];
         top = -1;
     }
 
-    public boolean isEmpty() {
+    /**
+     * Return true if the stack is empty
+     *
+     * @return boolean
+     */
+    boolean isEmpty() {
         return (top < 0);
     }
 
-    public String peek() {
+    /**
+     * Shows the String at the top of the stack
+     *
+     * @return String
+     */
+    String peek() {
         return (top < 0) ? null : stack[top];
     }
 
-    public String pop() {
+    /**
+     * Gets the String at the top of the stack and decreases the stack's size
+     *
+     * @return String
+     */
+    String pop() {
         if (top < 0)
             return null;
         String result = stack[top];
@@ -28,7 +48,12 @@ public class MyDynamicStack {
         return result;
     }
 
-    public void push(String value) {
+    /**
+     * Increases the size of the stack and adds a String to the top of the stack
+     *
+     * @param value     to be pushed to the top of the stack
+     */
+    void push(String value) {
         String[] temp = new String[top + 2];
         System.arraycopy(stack, 0, temp, 0, ++top);
         stack = temp;
