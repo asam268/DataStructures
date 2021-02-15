e2f8aec80bd5185a6d515095dc761cc6a39c9301
YIPPEEK1YAY <huntergiles2@gmail.com>
2021-02-15 11:38:13 -0500

    Queue test developed

--- Tests/whg/myQueueTest.java
+++ Tests/whg/myQueueTest.java
@@ -10,8 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class myQueueTest {
 
+    private myQueue a;
+
     @BeforeEach
     void setUp() {
+        a = new myQueue();
     }
 
     @AfterEach
@@ -20,21 +23,44 @@ class myQueueTest {
 
     @Test
     void publish() {
+        // it can publish a string;
+        a.publish("a");
+        assertEquals(1, a.size());
+
+        // it can publish more than one string;
+        a.publish("b");
+        assertEquals(2, a.size());
     }
 
     @Test
     void pull() {
+        //it returns 0 if queue is empty
+         assertNull(a.pull());
+
+        // can pull the last item in a queue of more than 1 entries
+        for (int i = 0; i <= 100; i++) {
+            String entry = String.valueOf(i);
+            a.publish(entry);
+        }
+        assertEquals("0", a.pull());
     }
 
     @Test
     void size() {
-    }
+        // size returns 0 if queue is empty
+        assertEquals(0, a.size());
 
-    @Test
-    void peekFront() {
-    }
+        // size increases by 1 when new entry is published
+        for (int i = 1; i <= 100; i++) {
+            String entry = String.valueOf(i);
+            a.publish(entry);
+            assertEquals(i, a.size());
+        }
 
-    @Test
-    void peekBack() {
+        // size decrease when entry is pulled
+        for (int i = 100; i >= 1; i--) {
+            assertEquals(i, a.size());
+            a.pull();
+        }
     }
 }
